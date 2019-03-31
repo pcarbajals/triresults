46d05b8a1679b548782fae9db064fc78e098b478
Pablo Carbajal <pablo@cover.com>
2019-03-31 15:25:02 -0400

    Completed Module #3 Summative: Implement Results Collection

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -11,5 +11,9 @@ class Entrant
 
   store_in collection: :results
   
-  embeds_many :results, class_name: 'LegResult', order: [:'event.o'.asc]
+  embeds_many :results, class_name: 'LegResult', order: [:'event.o'.asc], after_add: :update_total
+
+  def update_total(result)
+    self.secs = results.map { |a_result| a_result.secs }.reduce(:+)
+  end
 end
